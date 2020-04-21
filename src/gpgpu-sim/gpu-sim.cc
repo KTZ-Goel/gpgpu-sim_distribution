cb369da86f79330182e06adbddd9b78aed06dbdc
KTZ-Goel <goel46@qstruct.ecn.purdue.edu>
2020-04-21 18:13:29 -0400

    Bug

--- src/gpgpu-sim/gpu-sim.cc
+++ src/gpgpu-sim/gpu-sim.cc
@@ -2018,4 +2018,6 @@ const shader_core_config *gpgpu_sim::getShaderCoreConfig() {
 
 const memory_config *gpgpu_sim::getMemoryConfig() { return m_memory_config; }
 
-simt_core_cluster *gpgpu_sim::getSIMTCluster() { return *m_cluster; }
+simt_core_cluster *gpgpu_sim::getSIMTCluster(int i) {	//changed
+  return *(m_cluster+i);
+}
