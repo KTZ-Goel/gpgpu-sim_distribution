cc41ea24d9f0b7421234cd2ca81a4f87d92a3b53
Kshitiz Goel <kgoel62@gmail.com>
2020-04-23 01:49:05 -0400

    maybe.. WHo knows

--- src/gpgpu-sim/gpu-sim.cc
+++ src/gpgpu-sim/gpu-sim.cc
@@ -1705,7 +1705,7 @@ void gpgpu_sim::memunit_cycle()
       // The request is serviced.. Feed the mf to the upwards queue
       //int simt_cluster_id = mf->get_sid() / m_config.num_core_per_cluster();
       getSIMTCluster((*iter)->simtClusterID)->push_gmmu_cu_queue(mf);
-      latency_queue.pop_front();
+      latency_queue.remove(*iter);
     }
   }
 
