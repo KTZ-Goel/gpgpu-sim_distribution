f4812046fbea32f8195c95be07c3e776141ae44f
Kshitiz Goel <kgoel62@gmail.com>
2020-05-07 21:31:09 -0400

    Debug

--- src/gpgpu-sim/gpu-sim.cc
+++ src/gpgpu-sim/gpu-sim.cc
@@ -2015,11 +2015,11 @@ void gpgpu_sim::memunit_cycle()
               k++;           
               #ifdef PREFETCH_RANDOM
               // Calculate Prefetch Page
-              size_t random_size = ( rand() % 10) * 4096;
+              size_t random_size = ( rand() % 10);
 
               mem_addr_t prefetch_address =  random_size + get_global_memory()->get_page_num(mf->get_addr() + mf->get_access_size() - 1);   // Add code here
               struct prefetch_req  new_pref;
-              new_pref.start_addr = prefetch_address;
+              new_pref.start_addr = prefetch_address*4096;
               new_pref.size = 4096;
               new_pref.m_stream = NULL;
               new_pref.active = true;
